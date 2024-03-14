c4d94fb99bbc87db5b9ef85329c54fd30f8b29a3
Oldsmob1le <abdulin00@vk.com>
2024-03-14 15:03:28 +0300

    classwork

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,9 +1,11 @@
-import { LogoIcon } from '@/ui/icons'
-import React from 'react'
+'use client';
+import { LogoIcon, MenuIcon } from '@/ui/icons'
+import React, { useState } from 'react'
 import Image from 'next/image';
 import { Banner } from '@/ui/icons';
 
-const header = () => {
+const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const logoStyle = {
     display: 'flex',
@@ -15,31 +17,37 @@ const header = () => {
     cursor: 'pointer',
   };
 
-  return (
-    <div className="text-gray-1 flex w-full justify-between mt-xl">
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
-<Image
-    className="z-[-1]"
-    src={Banner}
-    alt="Banner"
-    layout="responsive"
-    objectFit="cover"
-    objectPosition="center"
-    style={{
-        position: 'absolute',
-        width: '100%',
-        height: '375px',
-        left: '-1px',
-        right: '-113.21px',
-        top: '-15px',
-    }}
-/>
-      
-      <div style={logoStyle} >
+  return (
+    <div className="text-gray-1 flex flex-col tablet-s:flex-row w-full justify-between mt-xl">
+
+      <Image
+        className="z-[-1]"
+        src={Banner}
+        alt="Banner"
+        layout="responsive"
+        objectFit="cover"
+        objectPosition="center"
+        style={{
+          position: 'absolute',
+          width: '100%',
+          height: '375px',
+          left: '-1px',
+          right: '-113.21px',
+          top: '-15px',
+        }}
+      />
+
+      <div style={logoStyle}>
         <LogoIcon />
-        <p className='opacity-0 mobile:opacity-[1]'>Sky Bridge</p>
+        <p className=''>Sky Bridge</p>
       </div>
-      <div className="flex items-center mobile:gap-xl mobile:text-h1">
+
+
+      <div className="hidden tablet-s:flex items-center gap-xl text-h1">
         <a href="#">Билеты</a>
         <a href="#">Расписание рейсов</a>
         <a href="#">Услуги</a>
@@ -47,8 +55,23 @@ const header = () => {
         <a href="#">Маршруты</a>
         <a href="#">Личный кабинет</a>
       </div>
+
+      <div className="tablet-s:hidden absolute top-7 right-7">
+        <button onClick={toggleMenu}>
+          <MenuIcon />
+        </button>
+      </div>
+
+      <div className={`flex tablet-s:hidden items-end mr-3xl  ${isMenuOpen ? 'flex-col' : 'hidden'}`}>
+        <a href="#" className="mb-xs">Билеты</a>
+        <a href="#" className="mb-xs">Услуги</a>
+        <a href="#" className="mb-xs">О нас</a>
+        <a href="#" className="mb-xs">Маршруты</a>
+        <a href="#" className="mb-xs">Расписание рейсов</a>
+        <a href="#" className="mb-xs">Личный кабинет</a>
+      </div>
     </div>
   )
 }
 
-export default header
+export default Header
--- src/ui/icons/index.ts
+++ src/ui/icons/index.ts
@@ -2,6 +2,7 @@ import LogoIcon from "@/ui/icons/logo.svg"
 import Banner from "@/ui/icons/banner.png"
 import Shuffle from "@/ui/icons/shuffle.svg"
 import PopularImg from "@/ui/icons/popular.png"
+import MenuIcon from "@/ui/icons/burger.svg"
 
 
-export { LogoIcon, Banner, Shuffle, PopularImg }
+export { LogoIcon, Banner, Shuffle, PopularImg, MenuIcon }
