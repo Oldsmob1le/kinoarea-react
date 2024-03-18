2538b30402590cf13c87c61d7d75151fcd1c4294
Oldsmob1le <abdulin00@vk.com>
2024-03-18 18:16:43 +0300

    fix 18.03/1

--- src/api/get_destination_options.php
+++ src/api/get_destination_options.php
@@ -1,6 +1,19 @@
 <?php
 // Подключение к базе данных
-require_once 'connect.php';
+header("Access-Control-Allow-Origin: *");
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "SKY"; // Не забудьте заменить на имя вашей базы данных
+
+// Создание соединения
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Проверка соединения
+if ($conn->connect_error) {
+    die("Не удалось подключиться: " . $conn->connect_error);
+}
 
 // Запрос к базе данных для получения вариантов для поля "Куда"
 $sql = "SELECT DISTINCT destination FROM flights";
