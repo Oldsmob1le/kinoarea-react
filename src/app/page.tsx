ab669e57b6c0b7751697075116137e45f702ed87
Oldsmob1le <abdulin00@vk.com>
2024-03-18 19:14:59 +0300

    fix 18.03

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 import Search from '@/components/main/search';
 import React from 'react';
 import Popular from '@/components/main/slider/popular';
-import Ticket from '@/components/main/ticket';
 
 export default function Home() {
   return (
       <>
         <Search />
-        <Ticket />
       </>
   );
 }
